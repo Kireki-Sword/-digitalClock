654498ee7d13e77126c1532337d881c28c5bec46
Kireki-Sword <286178854+Kireki-Sword@users.noreply.github.com>
2026-06-17 20:10:14 +0000

    add JS

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 function changeTime(){
 
     const date = new Date();
-    const hours = now.getHours();
-    const minutes = 
+    const hours = now.getHours().toString().padStart(2,0);
+    const minutes = now.getMinutes().toString().padStart(2,0);
+    const secs = now.getSeconds().toString().padStart(2,0);
+
+    newTime = `${hours}:${minutes}:${secs}`
+    document.getElementById('clock') = newTime
 }
+
+changeTime()
+setInterval(changeTime,1000)
